914f4067a819eda558a597790aab34c26ae66420
Iron Berny <61606251+IronProg@users.noreply.github.com>
2024-01-11 14:29:27 -0300

    Add files via upload

---
+++ segunda.js
@@ -0,0 +1,122 @@
+var exerciciosSegunda = [
+    {
+        "nome": "Supino Reto com Barra",
+        "dicas": [
+            "Contrair escápulas e músculos abdominais.",
+            "Costas Inteiras ou Superiores no Banco",
+            "Mãos 10-20cm mais abertas que o ombro",
+            "Pernas  <= 90°"
+        ],
+        "reps" : "2x10 10kg",
+        "img": "https://treinomestre.com.br/wp-content/uploads/2014/01/supino-reto.jpg.webp"
+    },
+    {
+        "nome": "Supino Inclinado com Barra Livre",
+        "dicas": [],
+        "reps" : "2x10 10kg",
+        "img": "https://treinomestre.com.br/wp-content/uploads/2016/10/supino-inclinado-com-barra.jpg.webp"
+    },
+    {
+        "nome": "Supino Inclinado com Halteres",
+        "dicas": [ "Máximo de 90º" ],
+        "reps" : "2x10 6kg",
+        "img": "https://treinomestre.com.br/wp-content/uploads/2016/10/supino-inclinado-com-halteres.jpg.webp"
+    },
+    {
+        "nome": "Supino Inclinado Guiado",
+        "dicas": [ "Máximo de 90º" ],
+        "reps" : "2x10 10kg",
+        "img": "https://treinomestre.com.br/wp-content/uploads/2016/10/supino-inclinado-com-halteres.jpg.webp"
+    },
+    {
+        "nome": "Flexão Aberta",
+        "dicas": [ "Alongar pulsos antes" ],
+        "reps" : "2x10 0kg",
+        "img": "https://treinomestre.com.br/wp-content/uploads/2017/08/flexao-de-bra%C3%A7o-push-up-varia%C3%A7%C3%B5es.jpg"
+    },
+    {
+        "nome": "Flexão Fechada de Joelhos",
+        "dicas": [ "Máximo de 90º" ],
+        "reps" : "2x10 0kg",
+        "img": "https://treinomestre.com.br/wp-content/uploads/2017/08/flexao-de-bra%C3%A7o-push-up-varia%C3%A7%C3%B5es.jpg"
+    },
+    {
+        "nome": "Tríceps com Barra Reta",
+        "dicas": [ "Levemente flexionado para a frente",
+            "Ombro levemente estendido",
+            "Escápulas Neutras",
+            "Mover apenas com o cotovelo",
+            "Fazer com controle e cadência lenta"
+        ],
+        "reps" : "2x10 25kg",
+        "img": "https://treinomestre.com.br/wp-content/uploads/2017/09/triceps-pulley-corda-polia.jpg"
+    },
+    {
+        "nome": "Tríceps com Corda",
+        "dicas": [ "Levemente flexionado para a frente",
+            "Ombro levemente estendido",
+            "Escápulas Neutras",
+            "Movimento de prece à dentro da cintura",
+            "Fazer com controle e cadência lenta"
+        ],
+        "reps" : "2x8 20kg",
+        "img": "https://grandeatleta.com.br/wp-content/uploads/2018/08/triceps-pulley-corda-na-polia.jpg"
+    },
+    {
+        "nome": "Tríceps Francês com Barra W",
+        "dicas": [ "Não mover os ombros",
+            "Mão dobrada na barra"
+        ],
+        "reps" : "Testar Pesagem",
+        "img": "https://www.cambiatufisico.com/wp-content/uploads/2011/02/Extensiones-declinadas-con-barra-Z.jpg"
+    },
+    {
+        "nome": "Tríceps Testa com Barra",
+        "dicas": [ "Até quase enconstar na testa",
+            "Mão dobrada se for usar barra W"
+        ],
+        "reps" : "2x10 6kg +",
+        "img": "https://treinomestre.com.br/wp-content/uploads/2019/03/triceps-testa.jpg"
+    },
+    {
+        "nome": "Frontal com Barra",
+        "dicas": [ "Até enconstar no peito",
+            "Estender as Mãos"
+        ],
+        "reps" : "Testar Peso",
+        "img": "https://static.wixstatic.com/media/2edbed_6b374d5a90614474bf75e82dce03ae81~mv2.webp"
+    },
+    {
+        "nome": "Frontal com Barra",
+        "dicas": [ "Até enconstar no peito",
+            "Estender as Mãos"
+        ],
+        "reps" : "Testar Peso",
+        "img": "https://static.wixstatic.com/media/2edbed_6b374d5a90614474bf75e82dce03ae81~mv2.webp"
+    },
+    {
+        "nome": "Elevação Lateral",
+        "dicas": [ "Postura Reta",
+            "Máximo de 90º"
+        ],
+        "reps" : "2x10 6kg",
+        "img": "https://static.wixstatic.com/media/2edbed_f79dd5050b0c42deb1fdc046d7108c7e~mv2.jpg/v1/fill/w_640,h_402,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/2edbed_f79dd5050b0c42deb1fdc046d7108c7e~mv2.jpg"
+    },
+    {
+        "nome": "Elevação Frontal com Halteres",
+        "dicas": [ "Postura Reta",
+            "Máximo de 90º"
+        ],
+        "reps" : "2x10 6kg",
+        "img": "https://www.elhombre.com.br/wp-content/uploads/2014/07/Dumbbell_Shoulder_Press_M_WorkoutLabs.png"
+    },
+    {
+        "nome": "Face Pull",
+        "dicas": [ "Altura do peito",
+            "Puxar até atravessar a Testa",
+            "Contrair trapézio e escápulas",
+        ],
+        "reps" : "2x10 6kg",
+        "img": "https://treinomestre.com.br/wp-content/uploads/2019/10/face-pull-exercicio.jpg"
+    },
+]
